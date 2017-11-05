bc75e0f80fa214b5a1de5c64da72ae5f08c1d4c4
liguangyi <252438823@qq.com>
2017-11-06 00:31:02 +0800

    update

--- App.js
+++ App.js
@@ -1,34 +1,12 @@
 import React from 'react';
-import MyButton from './Button.js';
+import Modal from './Modal';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      arr: [1],
-    }
-    this.onClick = this.onClick.bind(this);
-  }
-  onClick() {
-    this.setState({
-      arr: [...this.state.arr, 2],
-    });
-
-    // const arr = this.state.arr;
-    // arr.push(2);
-    // this.setState({
-    //   arr: arr,
-    // })
-  }
   render() {
     return (
-      <MyButton onClick={this.onClick} arr={this.state.arr} />
+      <Modal />
     );
   }
-}
-
-App.defaultProps = {
-  name: 'default'
 }
 
 export default App;
---
+++ Modal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Modal.css';
+
+class Modal extends React.Component {
+  render() {
+    return (
+      <div className="modal">Hello World</div>
+    )
+  }
+}
+
+export default Modal;
