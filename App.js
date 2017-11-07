e2007efb64b5f5bd9ce0638ffb7d7abb65aa3a43
liguangyi <252438823@qq.com>
2017-11-07 23:51:09 +0800

    update

--- App.js
+++ App.js
@@ -2,9 +2,30 @@ import React from 'react';
 import Modal from './Modal';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 1,
+    }
+  }
+  componentDidMount() {
+    window.setInterval(() => {
+      this.setState({
+        count: ++this.state.count,
+      })
+    }, 1000)
+  }
   render() {
+    const {count} = this.state;
+    const domArr = []
+    for (let i = 0; i < count; i++) {
+      domArr.push(<Modal key={i} />)
+    }
+    console.log('render:', count);
     return (
-      <Modal />
+      <div>
+        {domArr}        
+      </div>
     );
   }
 }
